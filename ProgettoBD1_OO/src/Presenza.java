4cda0a6b65fe389226f2995bffd815de8a423d23
Stefano <st.sabia@studenti.unina.it>
2022-01-28 10:13:19 +0100

    modifica Progetto

---
+++ ProgettoBD1_OO/src/Presenza.java
@@ -0,0 +1,36 @@
+
+public class Presenza extends Lezione {
+	private String nomeAula;
+	private Integer postiDisponibili;
+	private Integer postiTotali;
+	private String codiceAula;
+	
+	
+	public String getNomeAula() {
+		return nomeAula;
+	}
+	public void setNomeAula(String nomeAula) {
+		this.nomeAula = nomeAula;
+	}
+	public Integer getPostiDisponibili() {
+		return postiDisponibili;
+	}
+	public void setPostiDisponibili(Integer postiDisponibili) {
+		this.postiDisponibili = postiDisponibili;
+	}
+	public Integer getPostiTotali() {
+		return postiTotali;
+	}
+	public void setPostiTotali(Integer postiTotali) {
+		this.postiTotali = postiTotali;
+	}
+	public String getCodiceAula() {
+		return codiceAula;
+	}
+	public void setCodiceAula(String codiceAula) {
+		this.codiceAula = codiceAula;
+	}
+	
+	
+	
+}
