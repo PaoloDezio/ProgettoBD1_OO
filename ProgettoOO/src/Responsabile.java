efc8d29fdc3859b68a782f47fb8318902783dd0b
Stefano <st.sabia@studenti.unina.it>
2022-02-01 10:34:15 +0100

    Soluzione problema ProgettoOO.lnk

---
+++ ProgettoOO/src/Responsabile.java
@@ -0,0 +1,38 @@
+
+public class Responsabile extends Utente{
+	private Integer codiceResponsabile;
+	private String username;
+	private String password;
+	private String email;
+	
+	
+	
+	public Integer getCodiceResponsabile() {
+		return codiceResponsabile;
+	}
+	public void setCodiceResponsabile(Integer codiceResponsabile) {
+		this.codiceResponsabile = codiceResponsabile;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
---
+++ ProgettoOO/src/Utente.java
@@ -0,0 +1,45 @@
+
+public class Utente {
+	private String nome;
+	private String cognome;
+	private String luogoNascita;
+	private String dataNascita;
+	private String cf;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getLuogoNascita() {
+		return luogoNascita;
+	}
+	public void setLuogoNascita(String luogoNascita) {
+		this.luogoNascita = luogoNascita;
+	}
+	public String getDataNascita() {
+		return dataNascita;
+	}
+	public void setDataNascita(String dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+	public String getCf() {
+		return cf;
+	}
+	public void setCf(String cf) {
+		this.cf = cf;
+	}
+	
+	
+	
+	
+	
+}
