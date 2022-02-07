58dd0303234763009b00b1d5fa10423534bb6da8
Stefano <st.sabia@studenti.unina.it>
2022-02-07 20:08:48 +0100

    Modificato RicercaCorsoFrame

--- ProgettoOO/src/IscrizioneFrame.java
+++ ProgettoOO/src/IscrizioneFrame.java
@@ -13,11 +13,20 @@ import java.awt.Insets;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class IscrizioneFrame extends JFrame {
 
 	private JPanel contentPane;
 	private Controller controller;
+	private JTextField nomeTF;
+	private JTextField textField;
+	private JTextField matricolaTF;
+	private JTextField dataDiNascitaTF;
+	private JTextField luogoDiNascitaTF;
 	
 
 	/**
@@ -33,11 +42,131 @@ public class IscrizioneFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWidths = new int[]{70, 0, 100, 31, 0, 0, 0};
 		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel iscrizioneLabel = new JLabel("Iscrizione ");
+		iscrizioneLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		GridBagConstraints gbc_iscrizioneLabel = new GridBagConstraints();
+		gbc_iscrizioneLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_iscrizioneLabel.gridx = 1;
+		gbc_iscrizioneLabel.gridy = 1;
+		contentPane.add(iscrizioneLabel, gbc_iscrizioneLabel);
+		
+		JLabel nomeLabel = new JLabel("Nome");
+		nomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_nomeLabel = new GridBagConstraints();
+		gbc_nomeLabel.anchor = GridBagConstraints.WEST;
+		gbc_nomeLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_nomeLabel.gridx = 1;
+		gbc_nomeLabel.gridy = 3;
+		contentPane.add(nomeLabel, gbc_nomeLabel);
+		
+		nomeTF = new JTextField();
+		nomeTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_nomeTF = new GridBagConstraints();
+		gbc_nomeTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nomeTF.insets = new Insets(0, 0, 5, 5);
+		gbc_nomeTF.gridx = 2;
+		gbc_nomeTF.gridy = 3;
+		contentPane.add(nomeTF, gbc_nomeTF);
+		nomeTF.setColumns(10);
+		
+		JLabel cognomeLabel = new JLabel("Cognome");
+		cognomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_cognomeLabel = new GridBagConstraints();
+		gbc_cognomeLabel.anchor = GridBagConstraints.WEST;
+		gbc_cognomeLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_cognomeLabel.gridx = 1;
+		gbc_cognomeLabel.gridy = 4;
+		contentPane.add(cognomeLabel, gbc_cognomeLabel);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 4;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel matricolaLabel = new JLabel("Matricola");
+		matricolaLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_matricolaLabel = new GridBagConstraints();
+		gbc_matricolaLabel.anchor = GridBagConstraints.WEST;
+		gbc_matricolaLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_matricolaLabel.gridx = 1;
+		gbc_matricolaLabel.gridy = 5;
+		contentPane.add(matricolaLabel, gbc_matricolaLabel);
+		
+		matricolaTF = new JTextField();
+		matricolaTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_matricolaTF = new GridBagConstraints();
+		gbc_matricolaTF.insets = new Insets(0, 0, 5, 5);
+		gbc_matricolaTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_matricolaTF.gridx = 2;
+		gbc_matricolaTF.gridy = 5;
+		contentPane.add(matricolaTF, gbc_matricolaTF);
+		matricolaTF.setColumns(10);
+		
+		JLabel dataDiNascitaLabel = new JLabel("Data di Nascita");
+		dataDiNascitaLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_dataDiNascitaLabel = new GridBagConstraints();
+		gbc_dataDiNascitaLabel.anchor = GridBagConstraints.WEST;
+		gbc_dataDiNascitaLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_dataDiNascitaLabel.gridx = 1;
+		gbc_dataDiNascitaLabel.gridy = 6;
+		contentPane.add(dataDiNascitaLabel, gbc_dataDiNascitaLabel);
+		
+		dataDiNascitaTF = new JTextField();
+		dataDiNascitaTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_dataDiNascitaTF = new GridBagConstraints();
+		gbc_dataDiNascitaTF.insets = new Insets(0, 0, 5, 5);
+		gbc_dataDiNascitaTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_dataDiNascitaTF.gridx = 2;
+		gbc_dataDiNascitaTF.gridy = 6;
+		contentPane.add(dataDiNascitaTF, gbc_dataDiNascitaTF);
+		dataDiNascitaTF.setColumns(10);
+		
+		JLabel luogoDiNascitaLabel = new JLabel("Luogo di Nascita");
+		luogoDiNascitaLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_luogoDiNascitaLabel = new GridBagConstraints();
+		gbc_luogoDiNascitaLabel.anchor = GridBagConstraints.EAST;
+		gbc_luogoDiNascitaLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_luogoDiNascitaLabel.gridx = 1;
+		gbc_luogoDiNascitaLabel.gridy = 7;
+		contentPane.add(luogoDiNascitaLabel, gbc_luogoDiNascitaLabel);
+		
+		luogoDiNascitaTF = new JTextField();
+		luogoDiNascitaTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_luogoDiNascitaTF = new GridBagConstraints();
+		gbc_luogoDiNascitaTF.insets = new Insets(0, 0, 5, 5);
+		gbc_luogoDiNascitaTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_luogoDiNascitaTF.gridx = 2;
+		gbc_luogoDiNascitaTF.gridy = 7;
+		contentPane.add(luogoDiNascitaTF, gbc_luogoDiNascitaTF);
+		luogoDiNascitaTF.setColumns(10);
+		
+		JLabel codiceDelCorsoLabel = new JLabel("Codice del Corso");
+		codiceDelCorsoLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_codiceDelCorsoLabel = new GridBagConstraints();
+		gbc_codiceDelCorsoLabel.anchor = GridBagConstraints.EAST;
+		gbc_codiceDelCorsoLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_codiceDelCorsoLabel.gridx = 1;
+		gbc_codiceDelCorsoLabel.gridy = 8;
+		contentPane.add(codiceDelCorsoLabel, gbc_codiceDelCorsoLabel);
+		
+		JComboBox codiceCorsoCB = new JComboBox();
+		GridBagConstraints gbc_codiceCorsoCB = new GridBagConstraints();
+		gbc_codiceCorsoCB.insets = new Insets(0, 0, 5, 5);
+		gbc_codiceCorsoCB.fill = GridBagConstraints.HORIZONTAL;
+		gbc_codiceCorsoCB.gridx = 2;
+		gbc_codiceCorsoCB.gridy = 8;
+		contentPane.add(codiceCorsoCB, gbc_codiceCorsoCB);
 		
 		JButton tornaHomeButton = new JButton("Indietro");
 		tornaHomeButton.addActionListener(new ActionListener() {
@@ -49,9 +178,23 @@ public class IscrizioneFrame extends JFrame {
 		tornaHomeButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		GridBagConstraints gbc_tornaHomeButton = new GridBagConstraints();
 		gbc_tornaHomeButton.insets = new Insets(0, 0, 0, 5);
-		gbc_tornaHomeButton.gridx = 15;
+		gbc_tornaHomeButton.gridx = 4;
 		gbc_tornaHomeButton.gridy = 10;
 		contentPane.add(tornaHomeButton, gbc_tornaHomeButton);
+		
+		JButton confermaButton = new JButton("Conferma");
+		confermaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,"Iscrizione avvenuta con successo");
+				c.chiudiIscrizione();
+				c.apriHome();
+			}
+		});
+		confermaButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_confermaButton = new GridBagConstraints();
+		gbc_confermaButton.gridx = 5;
+		gbc_confermaButton.gridy = 10;
+		contentPane.add(confermaButton, gbc_confermaButton);
 	}
 
 }
--- ProgettoOO/src/RicercaCorsoFrame.java
+++ ProgettoOO/src/RicercaCorsoFrame.java
@@ -20,12 +20,19 @@ import javax.swing.JButton;
 import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
 
 public class RicercaCorsoFrame extends JFrame {
 
 	private JPanel contentPane;
 	private Controller controller;
-	private JTable CorsoTable;
+	private JTextField nomeTF;
+	private JTextField parolaChiaveTF;
+	private JTextField dataTF;
+	private JTable table;
 
 	/**
 	 * Create the frame.
@@ -35,81 +42,115 @@ public class RicercaCorsoFrame extends JFrame {
 		controller = c;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(375, 175, 600, 370);
+		setBounds(375, 175, 650, 575);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(30, 144, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{10, 0, 130, 130, 19, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 180, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{10, 100, 130, 90, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{50, 0, 0, 0, 0, 0, 0, 0, 0, 180, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel ricercaCorsoLabel = new JLabel("Ricerca Corso");
 		ricercaCorsoLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		GridBagConstraints gbc_ricercaCorsoLabel = new GridBagConstraints();
-		gbc_ricercaCorsoLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_ricercaCorsoLabel.anchor = GridBagConstraints.WEST;
 		gbc_ricercaCorsoLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_ricercaCorsoLabel.gridx = 1;
 		gbc_ricercaCorsoLabel.gridy = 0;
 		contentPane.add(ricercaCorsoLabel, gbc_ricercaCorsoLabel);
 		
-		JLabel selezionaCorsoLabel = new JLabel("Selezionare un corso:");
-		selezionaCorsoLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		GridBagConstraints gbc_selezionaCorsoLabel = new GridBagConstraints();
-		gbc_selezionaCorsoLabel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_selezionaCorsoLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_selezionaCorsoLabel.gridx = 1;
-		gbc_selezionaCorsoLabel.gridy = 2;
-		contentPane.add(selezionaCorsoLabel, gbc_selezionaCorsoLabel);
+		JLabel nomeLabel = new JLabel("Nome");
+		nomeLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		nomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		nomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_nomeLabel = new GridBagConstraints();
+		gbc_nomeLabel.anchor = GridBagConstraints.EAST;
+		gbc_nomeLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_nomeLabel.gridx = 1;
+		gbc_nomeLabel.gridy = 2;
+		contentPane.add(nomeLabel, gbc_nomeLabel);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
-		gbc_scrollPane.gridwidth = 3;
-		gbc_scrollPane.gridx = 1;
-		gbc_scrollPane.gridy = 3;
-		contentPane.add(scrollPane, gbc_scrollPane);
+		nomeTF = new JTextField();
+		nomeTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_nomeTF = new GridBagConstraints();
+		gbc_nomeTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nomeTF.insets = new Insets(0, 0, 5, 5);
+		gbc_nomeTF.gridx = 2;
+		gbc_nomeTF.gridy = 2;
+		contentPane.add(nomeTF, gbc_nomeTF);
+		nomeTF.setColumns(10);
 		
-		CorsoTable = new JTable();
-		CorsoTable.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		scrollPane.setViewportView(CorsoTable);
+		JLabel areaTematicaLabel = new JLabel("Area Tematica");
+		areaTematicaLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_areaTematicaLabel = new GridBagConstraints();
+		gbc_areaTematicaLabel.anchor = GridBagConstraints.EAST;
+		gbc_areaTematicaLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_areaTematicaLabel.gridx = 1;
+		gbc_areaTematicaLabel.gridy = 3;
+		contentPane.add(areaTematicaLabel, gbc_areaTematicaLabel);
 		
-		JButton ModificaButton = new JButton("Modifica");
-		ModificaButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		GridBagConstraints gbc_ModificaButton = new GridBagConstraints();
-		gbc_ModificaButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_ModificaButton.insets = new Insets(0, 0, 0, 5);
-		gbc_ModificaButton.gridx = 1;
-		gbc_ModificaButton.gridy = 4;
-		contentPane.add(ModificaButton, gbc_ModificaButton);
+		JComboBox areaTematicaCB = new JComboBox();
+		areaTematicaCB.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_areaTematicaCB = new GridBagConstraints();
+		gbc_areaTematicaCB.insets = new Insets(0, 0, 5, 5);
+		gbc_areaTematicaCB.fill = GridBagConstraints.HORIZONTAL;
+		gbc_areaTematicaCB.gridx = 2;
+		gbc_areaTematicaCB.gridy = 3;
+		contentPane.add(areaTematicaCB, gbc_areaTematicaCB);
 		
-		JButton EliminaButton = new JButton("Elimina");
-		EliminaButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		GridBagConstraints gbc_EliminaButton = new GridBagConstraints();
-		gbc_EliminaButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_EliminaButton.insets = new Insets(0, 0, 0, 5);
-		gbc_EliminaButton.gridx = 2;
-		gbc_EliminaButton.gridy = 4;
-		contentPane.add(EliminaButton, gbc_EliminaButton);
+		JLabel parolaChiaveLabel = new JLabel("Parola Chiave");
+		parolaChiaveLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_parolaChiaveLabel = new GridBagConstraints();
+		gbc_parolaChiaveLabel.anchor = GridBagConstraints.EAST;
+		gbc_parolaChiaveLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_parolaChiaveLabel.gridx = 1;
+		gbc_parolaChiaveLabel.gridy = 4;
+		contentPane.add(parolaChiaveLabel, gbc_parolaChiaveLabel);
 		
-		JButton StatisticheButton = new JButton("Statistische");
-		StatisticheButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		StatisticheButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		GridBagConstraints gbc_StatisticheButton = new GridBagConstraints();
-		gbc_StatisticheButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_StatisticheButton.insets = new Insets(0, 0, 0, 5);
-		gbc_StatisticheButton.gridx = 3;
-		gbc_StatisticheButton.gridy = 4;
-		contentPane.add(StatisticheButton, gbc_StatisticheButton);
+		parolaChiaveTF = new JTextField();
+		parolaChiaveTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_parolaChiaveTF = new GridBagConstraints();
+		gbc_parolaChiaveTF.insets = new Insets(0, 0, 5, 5);
+		gbc_parolaChiaveTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_parolaChiaveTF.gridx = 2;
+		gbc_parolaChiaveTF.gridy = 4;
+		contentPane.add(parolaChiaveTF, gbc_parolaChiaveTF);
+		parolaChiaveTF.setColumns(10);
 		
-		JButton tornaHomeButton = new JButton("Indietro");
+		JLabel dataLabel = new JLabel("Data");
+		dataLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_dataLabel = new GridBagConstraints();
+		gbc_dataLabel.anchor = GridBagConstraints.EAST;
+		gbc_dataLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_dataLabel.gridx = 1;
+		gbc_dataLabel.gridy = 5;
+		contentPane.add(dataLabel, gbc_dataLabel);
+		
+		dataTF = new JTextField();
+		dataTF.setToolTipText("01-01-22");
+		dataTF.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_dataTF = new GridBagConstraints();
+		gbc_dataTF.insets = new Insets(0, 0, 5, 5);
+		gbc_dataTF.fill = GridBagConstraints.HORIZONTAL;
+		gbc_dataTF.gridx = 2;
+		gbc_dataTF.gridy = 5;
+		contentPane.add(dataTF, gbc_dataTF);
+		dataTF.setColumns(10);
+		
+		JButton cercaButton = new JButton("Cerca");
+		cercaButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_cercaButton = new GridBagConstraints();
+		gbc_cercaButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cercaButton.insets = new Insets(0, 0, 5, 5);
+		gbc_cercaButton.gridx = 3;
+		gbc_cercaButton.gridy = 6;
+		contentPane.add(cercaButton, gbc_cercaButton);
+		
+		JButton tornaHomeButton = new JButton("Torna alla Home");
 		tornaHomeButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		tornaHomeButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -118,9 +159,58 @@ public class RicercaCorsoFrame extends JFrame {
 			}
 		});
 		GridBagConstraints gbc_tornaHomeButton = new GridBagConstraints();
-		gbc_tornaHomeButton.gridx = 5;
-		gbc_tornaHomeButton.gridy = 4;
+		gbc_tornaHomeButton.insets = new Insets(0, 0, 5, 0);
+		gbc_tornaHomeButton.gridx = 4;
+		gbc_tornaHomeButton.gridy = 6;
 		contentPane.add(tornaHomeButton, gbc_tornaHomeButton);
+		
+		JLabel selezionaUnCorsoLabel = new JLabel("Selezionare un corso:");
+		selezionaUnCorsoLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		GridBagConstraints gbc_selezionaUnCorsoLabel = new GridBagConstraints();
+		gbc_selezionaUnCorsoLabel.anchor = GridBagConstraints.WEST;
+		gbc_selezionaUnCorsoLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_selezionaUnCorsoLabel.gridx = 1;
+		gbc_selezionaUnCorsoLabel.gridy = 8;
+		contentPane.add(selezionaUnCorsoLabel, gbc_selezionaUnCorsoLabel);
+		
+		table = new JTable();
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.gridwidth = 3;
+		gbc_table.insets = new Insets(0, 0, 5, 5);
+		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.gridx = 1;
+		gbc_table.gridy = 9;
+		contentPane.add(table, gbc_table);
+		
+		JButton StatisticheButton = new JButton("Statistische");
+		StatisticheButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		StatisticheButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_StatisticheButton = new GridBagConstraints();
+		gbc_StatisticheButton.anchor = GridBagConstraints.WEST;
+		gbc_StatisticheButton.insets = new Insets(0, 0, 0, 5);
+		gbc_StatisticheButton.gridx = 1;
+		gbc_StatisticheButton.gridy = 10;
+		contentPane.add(StatisticheButton, gbc_StatisticheButton);
+		
+		JButton ModificaButton = new JButton("Modifica");
+		ModificaButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_ModificaButton = new GridBagConstraints();
+		gbc_ModificaButton.anchor = GridBagConstraints.WEST;
+		gbc_ModificaButton.insets = new Insets(0, 0, 0, 5);
+		gbc_ModificaButton.gridx = 2;
+		gbc_ModificaButton.gridy = 10;
+		contentPane.add(ModificaButton, gbc_ModificaButton);
+		
+		JButton EliminaButton = new JButton("Elimina");
+		EliminaButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_EliminaButton = new GridBagConstraints();
+		gbc_EliminaButton.insets = new Insets(0, 0, 0, 5);
+		gbc_EliminaButton.gridx = 3;
+		gbc_EliminaButton.gridy = 10;
+		contentPane.add(EliminaButton, gbc_EliminaButton);
 	}
 
 }
